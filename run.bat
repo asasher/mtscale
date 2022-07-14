74b43b1494ec99c7cae1a4708f6de72e104a7101
Asher Anjum <asheranjum@muhammad.anjum-Mac>
2022-07-14 16:05:11 +0400

    Update script to take outfile name as input

--- run.bat
+++ run.bat
@@ -1,5 +1,6 @@
 @ECHO OFF
+set /p outfile="Enter the name of the output file: "
 set /p refresh="Enter the refresh frequency in seconds: "
 CALL %~dp0\env\Scripts\activate
-python %~dp0\mtscale.py --refresh %refresh%
+python %~dp0\mtscale.py --out %outfile% --refresh %refresh%
 PAUSE
