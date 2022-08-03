138aefb466890e67437c6492c079966751e660ee
Asher <as.asher.anjum@gmail.com>
2022-08-03 17:20:06 +0400

    Update run to take port as input

--- run.bat
+++ run.bat
@@ -1,6 +1,7 @@
 @ECHO OFF
 set /p outfile="Enter the name of the output file: "
 set /p refresh="Enter the refresh frequency in seconds: "
+set /p port="Enter the port the device is connected to: "
 CALL %~dp0\env\Scripts\activate
-python %~dp0\mtscale.py --out %outfile% --refresh %refresh%
+python %~dp0\mtscale.py --out %outfile% --refresh %refresh% --port %port%
 PAUSE
